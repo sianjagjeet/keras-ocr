7bd1a40c705fb508f5150eb44a50c9698ff55873
sianjagjeet <112611385+sianjagjeet@users.noreply.github.com>
2024-04-28 11:59:11 +0530

    changed recognize function of pipeline

--- keras_ocr/pipeline.py
+++ keras_ocr/pipeline.py
@@ -25,51 +25,69 @@ class Pipeline:
         self.recognizer = recognizer
         self.max_size = max_size
 
-    def recognize(self, images, detection_kwargs=None, recognition_kwargs=None):
-        """Run the pipeline on one or multiples images.
-
-        Args:
-            images: The images to parse (can be a list of actual images or a list of filepaths)
-            detection_kwargs: Arguments to pass to the detector call
-            recognition_kwargs: Arguments to pass to the recognizer call
-
-        Returns:
-            A list of lists of (text, box) tuples.
-        """
-
+    def recognize(self, images, detection_kwargs=None):
         # Make sure we have an image array to start with.
         if not isinstance(images, np.ndarray):
             images = [tools.read(image) for image in images]
-        # This turns images into (image, scale) tuples temporarily
-        images = [
-            tools.resize_image(image, max_scale=self.scale, max_size=self.max_size)
-            for image in images
-        ]
-        max_height, max_width = np.array(
-            [image.shape[:2] for image, scale in images]
-        ).max(axis=0)
-        scales = [scale for _, scale in images]
-        images = np.array(
-            [
-                tools.pad(image, width=max_width, height=max_height)
-                for image, _ in images
-            ]
-        )
-        if detection_kwargs is None:
-            detection_kwargs = {}
-        if recognition_kwargs is None:
-            recognition_kwargs = {}
-        box_groups = self.detector.detect(images=images, **detection_kwargs)
-        prediction_groups = self.recognizer.recognize_from_boxes(
-            images=images, box_groups=box_groups, **recognition_kwargs
-        )
-        box_groups = [
-            tools.adjust_boxes(boxes=boxes, boxes_format="boxes", scale=1 / scale)
-            if scale != 1
-            else boxes
-            for boxes, scale in zip(box_groups, scales)
-        ]
-        return [
-            list(zip(predictions, boxes))
-            for predictions, boxes in zip(prediction_groups, box_groups)
-        ]
+            # This turns images into (image, scale) tuples temporarily
+            images = [tools.resize_image(image, max_scale=self.scale, max_size=self.max_size) for image in images]
+            max_height, max_width = np.array([image.shape[:2] for image, scale in images]).max(axis=0)
+            scales = [scale for _, scale in images]
+            images = np.array([tools.pad(image, width=max_width, height=max_height) for image, _ in images])
+            if detection_kwargs is None:
+                detection_kwargs = {}
+            box_groups = self.detector.detect(images=images, **detection_kwargs)
+            box_groups = [
+                tools.adjust_boxes(boxes=boxes, boxes_format="boxes", scale=1 / scale) if scale != 1 else boxes for
+                boxes, scale in zip(box_groups, scales)]
+        return [boxes.tolist() for boxes in box_groups]
+
+
+    # def recognize(self, images, detection_kwargs=None, recognition_kwargs=None):
+    #     """Run the pipeline on one or multiples images.
+
+    #     Args:
+    #         images: The images to parse (can be a list of actual images or a list of filepaths)
+    #         detection_kwargs: Arguments to pass to the detector call
+    #         recognition_kwargs: Arguments to pass to the recognizer call
+
+    #     Returns:
+    #         A list of lists of (text, box) tuples.
+    #     """
+
+    #     # Make sure we have an image array to start with.
+    #     if not isinstance(images, np.ndarray):
+    #         images = [tools.read(image) for image in images]
+    #     # This turns images into (image, scale) tuples temporarily
+    #     images = [
+    #         tools.resize_image(image, max_scale=self.scale, max_size=self.max_size)
+    #         for image in images
+    #     ]
+    #     max_height, max_width = np.array(
+    #         [image.shape[:2] for image, scale in images]
+    #     ).max(axis=0)
+    #     scales = [scale for _, scale in images]
+    #     images = np.array(
+    #         [
+    #             tools.pad(image, width=max_width, height=max_height)
+    #             for image, _ in images
+    #         ]
+    #     )
+    #     if detection_kwargs is None:
+    #         detection_kwargs = {}
+    #     if recognition_kwargs is None:
+    #         recognition_kwargs = {}
+    #     box_groups = self.detector.detect(images=images, **detection_kwargs)
+    #     prediction_groups = self.recognizer.recognize_from_boxes(
+    #         images=images, box_groups=box_groups, **recognition_kwargs
+    #     )
+    #     box_groups = [
+    #         tools.adjust_boxes(boxes=boxes, boxes_format="boxes", scale=1 / scale)
+    #         if scale != 1
+    #         else boxes
+    #         for boxes, scale in zip(box_groups, scales)
+    #     ]
+    #     return [
+    #         list(zip(predictions, boxes))
+    #         for predictions, boxes in zip(prediction_groups, box_groups)
+    #     ]
